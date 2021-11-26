d8e1079456bbf535c6606de3c02b80546c7b4656
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-26 11:28:14 +0000

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import bit2 from './bit2.png';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={bit2} className="App-logo img-fluid" alt="logo"/>
+        
+      </header>
+    </div>
+  );
+}
+
