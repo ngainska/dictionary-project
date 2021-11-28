ef3c93d23a5145caabb9fcab34ef76d8c3f4c384
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-28 20:19:56 +0000

    created keyword search form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import bit2 from './bit2.png';
 import './App.css';
+import Dictionary from './Dictionary';
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={bit2} className="App-logo img-fluid" alt="logo"/>
-        
       </header>
+      <main>
+        <Dictionary/>
+        </main>
+      <footer className="App-footer">
+        <small>Coded by Nicole Gainska</small></footer>
+      </div>
     </div>
   );
 }
