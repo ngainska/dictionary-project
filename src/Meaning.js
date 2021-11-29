b7cd7787ac95d2c4ba2ff1c1b6a8976d9878bac8
ngainska <87204580+ngainska@users.noreply.github.com>
2021-11-29 17:22:10 +0000

    Created synonym component

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Synonyms from "./Synonyms";
+
 
 export default function Meaning(props){
     console.log(props.meaning);
@@ -10,9 +12,13 @@ return (
     return(
 <div key={index}>
     <p>
+        <strong>Definition: </strong>
 {definition.definition}
 <br/>
+<strong>Example: </strong>
 <em>{definition.example}</em>
+<br/>
+<Synonyms synonyms={definition.synonyms}/>
 </p>
 </div>
     )
